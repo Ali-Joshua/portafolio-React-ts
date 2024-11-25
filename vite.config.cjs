eef2e8716f8d37ac519cd34ca722f4b07650fcc9
Ali Perez <aliperezkubi@hotmail.com>
2024-11-25 12:34:43 -0600

    ChangeCjsVite

--- vite.config.cjs
+++ vite.config.cjs
@@ -3,6 +3,4 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const vite_1 = require("vite");
 const plugin_react_swc_1 = require("@vitejs/plugin-react-swc");
 // https://vite.dev/config/
-exports.default = (0, vite_1.defineConfig)({
-    plugins: [(0, plugin_react_swc_1.default)()],
-});
+exports.default = require("vite").defineConfig({});
